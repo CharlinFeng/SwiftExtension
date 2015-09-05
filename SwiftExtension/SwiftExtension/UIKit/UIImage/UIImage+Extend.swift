af9fab773b9e79df93e787215aee5ed7b02f28f2
Charlin_冯成林 <1761904945@qq.com>
2015-09-05 09:51:09 +0800

    update

--- SwiftExtension/SwiftExtension/UIKit/UIImage/UIImage+Extend.swift
+++ SwiftExtension/SwiftExtension/UIKit/UIImage/UIImage+Extend.swift
@@ -6,9 +6,28 @@
 //  Copyright (c) 2015年 冯成林. All rights reserved.
 //
 
-import Foundation
+
 import UIKit
+
 extension UIImage {
     
+    func imageWithTintColor(tintColor: UIColor)->UIImage{
+        
+        UIGraphicsBeginImageContextWithOptions(self.size, false, 0.0);
+        
+        tintColor.setFill()
+    
+        let bounds = CGRectMake(0, 0, self.size.width, self.size.height)
+        
+        UIRectFill(bounds)
+        
+        self.drawInRect(bounds, blendMode: kCGBlendModeDestinationIn, alpha: 1.0)
+        
+        let tintedImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return tintedImage
+    }
     
 }
